2d1cbcd46358f9ff6ba3d4585df488b077d1bb5a
root <root@ip-172-31-44-28.ap-northeast-2.compute.internal>
2022-02-10 05:52:16 +0000

    test.py fixed

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import requests
 import os
 import json
 import sys
+from datetime import datetime
 
-# url = "https://itunes.apple.com/kr/rss/customerreviews/id=1435757638/xml"
+# url = "https://itunes.apple.com/kr/rss/customerreviews/id=1435566757638/xml"
 
 
 def appstore_crawler(url):
@@ -15,20 +16,29 @@ def appstore_crawler(url):
     result = []
     response = requests.get(url).content.decode('utf8')
     xml = xmltodict.parse(response)
-    for i in range(len(xml['feed']['entry'])):
-        review_pre = xml['feed']['entry'][i]['content'][0]['#text'].replace("\n", " ")
-        result.append({
-            'USER': xml['feed']['entry'][i]['author']['name'],
-            'DATE': xml['feed']['entry'][i]['updated'],
-            'STAR': int(xml['feed']['entry'][i]['im:rating']),
-            'LIKE': int(xml['feed']['entry'][i]['im:voteSum']),
-            'TITLE': xml['feed']['entry'][i]['title'],
-            # 'REVIEW': xml['feed']['entry'][i]['content'][0]['#text']
-            'REVIEW': review_pre
-        })
-    print(result)
-    res_df = pd.DataFrame(result)
-    res_df.to_csv("./appstore_reviews.csv", encoding='utf-8-sig', index=False)
+
+    while(True):
+        try:
+            for i in range(len(xml['feed']['entry'])):
+                review_fixed = xml['feed']['entry'][i]['content'][0]['#text'].replace("\n", " ")
+                result.append({
+                    'USER': xml['feed']['entry'][i]['author']['name'],
+                    'DATE': xml['feed']['entry'][i]['updated'],
+                    'STAR': int(xml['feed']['entry'][i]['im:rating']),
+                    'LIKE': int(xml['feed']['entry'][i]['im:voteSum']),
+                    'TITLE': xml['feed']['entry'][i]['title'],
+                    # 'REVIEW': xml['feed']['entry'][i]['content'][0]['#text']
+                    'REVIEW': review_fixed
+                })
+
+            print(result)
+            res_df = pd.DataFrame(result)
+            res_df.to_csv("./appstore_reviews.csv", encoding='utf-8-sig', index=False)
+
+        except KeyError:
+            print("Wrong URL")
+            return
+
 
 url = sys.argv[1]
 
